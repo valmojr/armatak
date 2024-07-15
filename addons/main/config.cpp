ec7345c35b1ca2209098653f8a3ddd619ed2512a
Valmo Trindade <val_mo_jr@hotmail.com>
2024-07-14 23:54:15 -0300

    added initial main addon on core mod

---
+++ addons/main/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+  class braf_main {
+    units[] = {""};
+    weapons[] = {""};
+    author = "ARMATAK Team";
+    url = "https://github.com/valmojr/armatak";
+    requiredAddons[] =
+      {
+        "cba_common"
+      };
+    requiredVersion = 0.1;
+  };
+};
