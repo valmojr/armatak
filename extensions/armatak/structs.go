9c987747df44dc6b3c73dece5ae8313e8dc00d12
Valmo Trindade <val_mo_jr@hotmail.com>
2024-07-23 19:27:15 -0300

    fixed GeoObject struct to handle json marshalling

--- extensions/armatak/structs.go
+++ extensions/armatak/structs.go
@@ -2,5 +2,18 @@ package main
 
 type Payload struct {
 	Content  string `json:"content"`
-	Username string `json:"username,omitempty"` // Optional field
+	Username string `json:"username,omitempty"`
+}
+
+type GeoObject struct {
+	UID       string  `json:"uid,omitempty"`
+	Longitude float32 `json:"longitude"`
+	Latitude  float32 `json:"latitude"`
+	Attitude  string  `json:"attitude"`
+	Bearing   int     `json:"bearing,omitempty"`
+	Distance  int     `json:"distance,omitempty"`
+	GeoObject string  `json:"geoObject"`
+	How       string  `json:"how,omitempty"`
+	Name      string  `json:"name,omitempty"`
+	Timeout   int     `json:"timeout,omitempty"`
 }
