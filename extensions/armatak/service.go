1e0591b809dd0f3925d6b621f50f3131ad95cc01
Valmo Trindade <val_mo_jr@hotmail.com>
2024-07-23 19:29:11 -0300

    [WIP] correct struct parsed on postGeoObject service, will have to finish real location handling to continue the tests on real FTS API, using a mocked backend for now

--- extensions/armatak/service.go
+++ extensions/armatak/service.go
@@ -1,10 +1,42 @@
 package main
 
+import "strconv"
+
 func armatak_service_ManageAPI_getHelp() (string, error) {
-	return getRequest("manageAPI/getHelp")
+	return getRequest("manageAPI/getHelp") // ERROR - returning unsuported on 2.2 FTS (API is returning this, not my fault)
 }
 
-func armatak_service_ManageGeoObject_postGeoObject() {}
+func armatak_service_ManageGeoObject_postGeoObject(args []string) (string, error) {
+	longitude, longitudeError := strconv.ParseFloat(args[1], 32)
+
+	if longitudeError != nil {
+		return "", longitudeError
+	}
+
+	latitude, latitudeError := strconv.ParseFloat(args[2], 32)
+
+	if latitudeError != nil {
+		return "", latitudeError
+	}
+
+	bearing, bearingError := strconv.Atoi(args[3])
+
+	if bearingError != nil {
+		return "", bearingError
+	}
+
+	payload := GeoObject{
+		UID:       args[0],
+		Longitude: float32(longitude),
+		Latitude:  float32(latitude),
+		Attitude:  "friendly",
+		Bearing:   bearing,
+		GeoObject: args[4],
+		Name:      args[5],
+	}
+
+	return postRequest("http://localhost:3000/ManageGeoObject/postGeoObject", payload)
+}
 
 func armatak_service_ManageGeoObject_putGeoObject() {}
 
