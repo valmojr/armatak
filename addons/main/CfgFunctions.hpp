3a1bd8c66431a09aae80e58b5cd776549b847f48
Valmo Trindade <val_mo_jr@hotmail.com>
2024-07-24 05:08:55 -0300

    added the new functions to CfgFunctions

--- addons/main/CfgFunctions.hpp
+++ addons/main/CfgFunctions.hpp
@@ -13,8 +13,18 @@ class CfgFunctions {
 			class extract_role {
 				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_role.sqf";
 			};
+			class extract_side {
+				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_side.sqf";
+			};
 			class extract_info {
 				file = "\armatak\armatak\armatak_main\functions\fn_extract_info.sqf";
+			};
+
+			class convert_location {
+				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_location.sqf";
+			};
+			class convert_to_altis {
+				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_altis.sqf";
 			};
 		};
 	};
