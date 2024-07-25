b3dc4281e3c7efa3659d33acecd4c52767f0c59e
Valmo Trindade <val_mo_jr@hotmail.com>
2024-07-24 23:53:51 -0300

    added putRequest method to utils

--- extensions/armatak/util.go
+++ extensions/armatak/util.go
@@ -77,3 +77,43 @@ func postRequest(route string, body any) (string, error) {
 
 	return string(parsedBody), nil
 }
+
+func putRequest(route string, body any) (string, error) {
+	jsonData, err := json.Marshal(body)
+	if err != nil {
+		fmt.Println("Error marshalling payload:", err)
+		return "", err
+	}
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodPut, route, bytes.NewReader(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	req.Header.Set("Authorization", "Bearer token")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error sending request:", resp.Status)
+		return "", nil
+	}
+
+	parsedBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("getting Error reading response body: %w", err)
+	}
+
+	return string(parsedBody), nil
+}
