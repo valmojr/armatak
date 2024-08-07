7f54a119ed762ca0c6ac0d882ba04a50554ae818
Valmo Trindade <val_mo_jr@hotmail.com>
2024-08-06 22:39:13 -0300

    fixed hemtt post build hook

---
+++ addons/items/CfgVehicles.hpp
@@ -0,0 +1,7 @@
+// Samsung S24 with Juggernault Case for vest as backpack item
+
+// Persistent Systems RDC for vest as backpack item
+
+// Samsung S24 with Juggernault Case handheld as backpack item
+
+// Persistent Systems RDC handheld as backpack item
---
+++ addons/items/config.cpp
@@ -0,0 +1,15 @@
+class CfgPatches {
+  class armatak_items {
+    units[] = {};
+    weapons[] = {};
+    author = "Valmo";
+    url = "https://github.com/valmojr/armatak";
+    requiredAddons[] =
+      {
+        "cba_common"
+      };
+    requiredVersion = 0.5;
+  };
+};
+
+#include "CfgVehicles.hpp"
