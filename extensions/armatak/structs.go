5b1890d0cf1b3827a6d403938aaecc74febf5136
Valmo Trindade <val_mo_jr@hotmail.com>
2024-08-17 15:31:19 -0300

    refactored Marker struct

--- extensions/armatak/structs.go
+++ extensions/armatak/structs.go
@@ -8,14 +8,13 @@ type Payload struct {
 type Marker struct {
 	Longitude float64 `json:"longitude"`
 	Latitude  float64 `json:"latitude"`
-	Attitude  string  `json:"attitude"`
 	Name      string  `json:"name"`
 	UID       string  `json:"uid"`
 	Type      string  `json:"type,omitempty"`
-	Course    float64 `json:"course,omitempty"`
-	Azimuth   float64 `json:"azimuth,omitempty"`
-	Speed     float64 `json:"speed,omitempty"`
-	Battery   float64 `json:"battery,omitempty"`
+	Course    int     `json:"course,omitempty"`
+	Azimuth   int     `json:"azimuth,omitempty"`
+	Speed     int     `json:"speed,omitempty"`
+	Battery   int     `json:"battery,omitempty"`
 	FOV       int     `json:"fov,omitempty"`
 	CE        int     `json:"ce,omitempty"`
 	HAE       int     `json:"hae,omitempty"`
