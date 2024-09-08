6efc4fb370aa9bf40036565fe43d74d9960aff70
Valmo Trindade <val_mo_jr@hotmail.com>
2024-09-08 15:20:11 -0300

    added unit tests for the uuid4 generator

--- src/tests.rs
+++ src/tests.rs
@@ -1,12 +1,25 @@
 #[cfg(test)]
 mod tests {
-    use crate::init;
+    mod uuid_tests {
+        use uuid::Uuid;
+        use crate::init;
 
-    #[test]
-    fn uuid_is_defined() {
-        let extension = init().testing();
-        let (output, _) = extension.call("uuid", None);
-        assert_eq!(output, output.to_string())
+        #[test]
+        fn uuid_output_is_string() {
+            let extension = init().testing();
+            let (output, _) = extension.call("uuid", None);
+            assert_eq!(output, output.to_string())
+        }
+
+        #[test]
+        fn uuid_output_is_uuid4_identifier() {
+            let extension = init().testing();
+            let (output, _) = extension.call("uuid", None);
+
+            let parsed_uuid = Uuid::parse_str(&output);
+            assert!(parsed_uuid.is_ok());
+            assert_eq!(parsed_uuid.unwrap().get_version(), Some(uuid::Version::Random))
+        }
     }
 
     mod markers_tests {
