386de182d3e816c2f3d701465f36971cb708e858
Valmo Trindade <val_mo_jr@hotmail.com>
2024-09-18 19:07:57 -0300

    removed request templates on util file, don't think i will be using it

--- src/util.rs
+++ src/util.rs
@@ -3,15 +3,3 @@ pub fn get_uuid() -> String {
 
     Uuid::new_v4().to_string()
 }
-
-mod sync_request {
-    pub fn get(address: String, token: String) -> String {
-        let par = address + &token;
-
-        return par;
-    }
-
-    pub fn post(data: String) -> &'static str {
-        return "not implemented yet";
-    }
-}
