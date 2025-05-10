9bb4a8feaa773abb02e9a14f4001a56477d0d884
Valmo Trindade <val_mo_jr@hotmail.com>
2025-05-10 05:23:28 -0300

    added 3den attributes for callsign, group color, roles, and marker types

--- addons/main/Cfg3den.hpp
+++ addons/main/Cfg3den.hpp
@@ -4,20 +4,18 @@ class Cfg3den {
       class armatak_3den_attributes {
         displayName = "ARMA Team Awareness Kit";
         class Attributes {
-          class armatak_attribute_unit_callsign {
-            displayName = "Callsign";
+          class armatak_attribute_callsign {
+            displayName = "Unit Callsign";
             tooltip = "Unit callsign on TAK";
-            property = "armatak_attribute_unit_callsign";
+            property = "armatak_attribute_callsign";
             control = "Edit";
-            value = "";
-            expression = "_this setVariable ['%s',_value];";
-            wikiType="[[String]]";
-            unique = 0;
+            expression = "_this setVariable ['armatak_attribute_callsign',_value]";
+            defaultValue = "''";
             condition = "object";
             typeName = "STRING";
           };
           class armatak_attribute_unit_role {
-            displayName = "Role";
+            displayName = "Unit Role";
             tooltip = "Unit role on TAK";
             property = "armatak_attribute_unit_role";
             control = "Combo";
@@ -59,6 +57,91 @@ class Cfg3den {
             expression = "_this setVariable ['%s',_value];";
             unique = 0;
             condition = "objectBrain";
+          };
+          class armatak_attribute_marker_type {
+            displayName = "Marker Type";
+            tooltip = "Marker type on TAK";
+            property = "armatak_attribute_marker_type";
+            control = "Edit";
+            expression = "_this setVariable ['armatak_attribute_marker_type',_value]";
+            defaultValue = "''";
+            condition = "objectVehicle";
+            typeName = "STRING";
+          };
+        };
+      };
+    };
+  };
+  class Group {
+    class AttributeCategories {
+      class armatak_3den_group_attributes {
+        displayName = "ARMA Team Awareness Kit";
+        class Attributes {
+          class armatak_attribute_group_color {
+            displayName = "Color";
+            tooltip = "Group on TAK";
+            property = "armatak_attribute_group_color";
+            control = "Combo";
+            class Values {
+              class armatak_attribute_group_color_value_white {
+                name = "White";
+                value = "White";
+              };
+              class armatak_attribute_group_color_value_yellow {
+                name = "Yellow";
+                value = "Yellow";
+              };
+              class armatak_attribute_group_color_value_orange {
+                name = "Orange";
+                value = "Orange";
+              };
+              class armatak_attribute_group_color_value_magenta {
+                name = "Magenta";
+                value = "Magenta";
+              };
+              class armatak_attribute_group_color_value_red {
+                name = "Red";
+                value = "Red";
+              };
+              class armatak_attribute_group_color_value_maroon {
+                name = "Maroon";
+                value = "Maroon";
+              };
+              class armatak_attribute_group_color_value_purple {
+                name = "Purple";
+                value = "Purple";
+              };
+              class armatak_attribute_group_color_value_darkblue {
+                name = "Dark Blue";
+                value = "DarkBlue";
+              };
+              class armatak_attribute_group_color_value_blue {
+                name = "Blue";
+                value = "Blue";
+              };
+              class armatak_attribute_group_color_value_cyan {
+                name = "Cyan";
+                value = "Cyan";
+              };
+              class armatak_attribute_group_color_value_teal {
+                name = "Teal";
+                value = "Teal";
+              };
+              class armatak_attribute_group_color_value_green {
+                name = "Green";
+                value = "Green";
+              };
+              class armatak_attribute_group_color_value_darkgreen {
+                name = "Dark Green";
+                value = "DarkGreen";
+              };
+              class armatak_attribute_group_color_value_brown {
+                name = "Brown";
+                value = "Brown";
+              };
+            };
+            expression = "_this setVariable ['%s',_value];";
+            unique = 0;
           };
         };
       };
