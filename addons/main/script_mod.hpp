39f4936bf7f4c6208133b0dffd3def54ecb85bc9
Valmo Trindade <val_mo_jr@hotmail.com>
2025-05-14 05:07:45 -0300

    added defines for main addon

---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,25 @@
+// COMPONENT should be defined in the script_component.hpp and included BEFORE this hpp
+
+#define MAINPREFIX armatak
+#define PREFIX armatak
+
+#include "script_version.hpp"
+
+#define VERSION     MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
+
+#define PROJECT_AUTHOR QUOTE(Valmo Trindade)
+
+// MINIMAL required version for the Mod. Components can specify others..
+#define REQUIRED_VERSION 2.18
+#define REQUIRED_CBA_VERSION {3,18,2}
+
+#ifndef COMPONENT_BEAUTIFIED
+    #define COMPONENT_BEAUTIFIED COMPONENT
+#endif
+#ifdef SUBCOMPONENT_BEAUTIFIED
+    #define COMPONENT_NAME QUOTE(ARMATAK - COMPONENT_BEAUTIFIED - SUBCOMPONENT_BEAUTIFIED)
+#else
+    #define COMPONENT_NAME QUOTE(ARMATAK - COMPONENT_BEAUTIFIED)
+#endif
--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define build 0
 #define major 1
-#define minor 0
+#define minor 1
 #define patch 0
