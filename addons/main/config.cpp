5231dae17a4410e5d96ae0e3f14aba01d47bf56e
Valmo Trindade <val_mo_jr@hotmail.com>
2025-05-14 05:07:05 -0300

    added CfgMods to main config

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,18 +1,40 @@
+#include "script_component.hpp"
+
 class CfgPatches {
-  class armatak_main {
+  class ADDON {
+    name = COMPONENT_NAME;
     units[] = {
       "armatak_module_core",
       "armatak_module_callsign"
     };
-    weapons[] = {""};
-    author = "Valmo";
-    url = "https://github.com/valmojr/armatak";
+    weapons[] = {};
     requiredAddons[] = {
         "cba_main",
         "ace_main"
       };
-    requiredVersion = 2.06;
+    requiredVersion = REQUIRED_VERSION;
+    author = QUOTE(Valmo);
+    url = "https://github.com/valmojr/armatak";
   };
+};
+
+class CfgMods {
+    class PREFIX {
+      name = "Arma Team Awareness Kit";
+      author = PROJECT_AUTHOR;
+      logo = "logo_ca.paa";
+      logoOver = "logo_hover_ca.paa";
+      tooltip	= "ARMATAK";
+      tooltipOwned = "ARMATAK";
+      picture = "picture.paa";
+      actionName = "GitHub";
+      action = "https://github.com/valmojr/armatak";
+      overview = "ARMATAK Addons allows Arma 3 sessions to be parsed to TAK Clients";
+      hideName = 0;
+      hidePicture	= 0;
+      dlcColor[] = { 0.23, 0.39, 0.30, 1 };
+      logoSmall	= "logo_small_ca.paa";
+    };
 };
 
 class Extended_PostInit_EventHandlers {
