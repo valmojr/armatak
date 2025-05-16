8a6dcff05df0c286854f5cc9fa3e6aefc5a5adf0
Valmo Trindade <val_mo_jr@hotmail.com>
2025-05-16 00:25:24 -0300

    added notify function for DRY

--- addons/main/CfgFunctions.hpp
+++ addons/main/CfgFunctions.hpp
@@ -2,105 +2,108 @@ class CfgFunctions {
 	class armatak {
 		class functions {
 			class handleCallbacks {
-				file = "\armatak\armatak\armatak_main\functions\fn_handleCallbacks.sqf";
+				file = "\armatak\armatak\addons\main\functions\fn_handleCallbacks.sqf";
 			};
 			class init {
-				file = "\armatak\armatak\armatak_main\functions\fn_init.sqf";
-			};
-			class video_init {
-				file = "\armatak\armatak\armatak_main\functions\fn_video_init.sqf";
+				file = "\armatak\armatak\addons\main\functions\fn_init.sqf";
 			};
 			class log_message {
-				file = "\armatak\armatak\armatak_main\functions\fn_log_message.sqf";
+				file = "\armatak\armatak\addons\main\functions\fn_log_message.sqf";
+			};
+			class video_init {
+				file = "\armatak\armatak\addons\main\functions\fn_video_init.sqf";
+			};
+			class notify {
+				file = "\armatak\armatak\addons\main\functions\fnc_notify.sqf";
 			};
 			class send_digital_pointer_cot {
-				file = "\armatak\armatak\armatak_main\functions\api\fn_send_digital_pointer_cot.sqf";
+				file = "\armatak\armatak\addons\main\functions\api\fn_send_digital_pointer_cot.sqf";
 			};
 			class send_drone_cot {
-				file = "\armatak\armatak\armatak_main\functions\api\fn_send_drone_cot.sqf";
+				file = "\armatak\armatak\addons\main\functions\api\fn_send_drone_cot.sqf";
 			};
 			class send_group_cots {
-				file = "\armatak\armatak\armatak_main\functions\api\fn_send_group_cots.sqf";
+				file = "\armatak\armatak\addons\main\functions\api\fn_send_group_cots.sqf";
 			};
 			class send_eud_cot {
-				file = "\armatak\armatak\armatak_main\functions\api\fn_send_eud_cot.sqf";
+				file = "\armatak\armatak\addons\main\functions\api\fn_send_eud_cot.sqf";
 			};
 			class send_marker_cot {
-				file = "\armatak\armatak\armatak_main\functions\api\fn_send_marker_cot.sqf";
+				file = "\armatak\armatak\addons\main\functions\api\fn_send_marker_cot.sqf";
 			};
 			class stop_tcp_socket {
-				file = "\armatak\armatak\armatak_main\functions\api\fn_stop_tcp_socket.sqf";
+				file = "\armatak\armatak\addons\main\functions\api\fn_stop_tcp_socket.sqf";
 			};
 			class extract_group_color {
-				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_group_color.sqf";
+				file = "\armatak\armatak\addons\main\functions\extract_data\fn_extract_group_color.sqf";
 			};
 			class extract_group_role {
-				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_group_role.sqf";
+				file = "\armatak\armatak\addons\main\functions\extract_data\fn_extract_group_role.sqf";
 			};
 			class extract_marker_callsign {
-				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_marker_callsign.sqf";
+				file = "\armatak\armatak\addons\main\functions\extract_data\fn_extract_marker_callsign.sqf";
 			};
 			class extract_role {
-				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_role.sqf";
+				file = "\armatak\armatak\addons\main\functions\extract_data\fn_extract_role.sqf";
 			};
 			class extract_unit_callsign {
-				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_unit_callsign.sqf";
+				file = "\armatak\armatak\addons\main\functions\extract_data\fn_extract_unit_callsign.sqf";
 			};
 			class extract_uuid {
-				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_extract_uuid.sqf";
+				file = "\armatak\armatak\addons\main\functions\extract_data\fn_extract_uuid.sqf";
 			};
 			class shorten_name {
-				file = "\armatak\armatak\armatak_main\functions\extract_data\fn_shorten_name.sqf";
+				file = "\armatak\armatak\addons\main\functions\extract_data\fn_shorten_name.sqf";
 			};
 
 			class ZeusCoreModule_show {
-				file = "\armatak\armatak\armatak_main\functions\gui\fn_ZeusCoreModule_show.sqf";
+				file = "\armatak\armatak\addons\main\functions\gui\fn_ZeusCoreModule_show.sqf";
 			};
 			class ZeusCoreModule_configure {
-				file = "\armatak\armatak\armatak_main\functions\gui\fn_ZeusCoreModule_configure.sqf";
+				file = "\armatak\armatak\addons\main\functions\gui\fn_ZeusCoreModule_configure.sqf";
 			};
 
 			class convert_to_altis {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_altis.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_altis.sqf";
 			};
 			class convert_to_bukovina {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_bukovina.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_bukovina.sqf";
 			};
 			class convert_to_bystrika {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_bystrika.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_bystrika.sqf";
 			};
 			class convert_to_chernarus {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_chernarus.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_chernarus.sqf";
 			};
 			class convert_to_cucui {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_cucui.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_cucui.sqf";
 			};
 			class convert_to_kunduz {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_kunduz.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_kunduz.sqf";
 			};
 			class convert_to_livonia {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_livonia.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_livonia.sqf";
 			};
 			class convert_to_malden {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_malden.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_malden.sqf";
 			};
 			class convert_to_southen_sahrani {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_southen_sahrani.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_southen_sahrani.sqf";
 			};
 			class convert_to_stratis {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_stratis.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_stratis.sqf";
 			};
 			class convert_to_takistan_montains {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_takistan_montains.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_takistan_montains.sqf";
 			};
 			class convert_to_tanoa {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_tanoa.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_tanoa.sqf";
 			};
 			class convert_to_united_sahrani {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_united_sahrani.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_united_sahrani.sqf";
 			};
 			class convert_to_vr {
-				file = "\armatak\armatak\armatak_main\functions\map\fn_convert_to_vr.sqf";
+				file = "\armatak\armatak\addons\main\functions\map\fn_convert_to_vr.sqf";
 			};
 		};
 	};
