c1f61dc4aa4ea45c0c744efa2aa10a854a637455
Valmo Trindade <val_mo_jr@hotmail.com>
2025-05-22 23:12:36 -0300

    Added initial custom marker dialog and fixed core module background

--- addons/server/dialog.hpp
+++ addons/server/dialog.hpp
@@ -4,6 +4,7 @@ class RscFrame;
 class RscLine;
 class RscProgress;
 class RscPicture;
+class RscBackground;
 class RscPictureKeepAspect;
 class RscVideo;
 class RscHTML;
@@ -39,13 +40,13 @@ class armatak_zeus_core_module_dialog {
   idd = 999991;
   movingEnable = 0;
   class ControlsBackground {
-    class armatak_gui_module_zeus_core_dialog_main_frame: RscFrame {
+    class armatak_gui_module_zeus_core_dialog_main_frame: RscBackground {
       idc = 1800;
       x = "0.386562 * safezoneW + safezoneX";
       y = "0.401 * safezoneH + safezoneY";
       w = "0.216563 * safezoneW";
       h = "0.242 * safezoneH";
-      colorBackground[]={0,0,0,0.5};
+      colorBackground[]={0,0,0,0.45};
     };
   };
   class Controls {
@@ -56,6 +57,7 @@ class armatak_zeus_core_module_dialog {
       y = "0.445 * safezoneH + safezoneY";
       w = "0.20625 * safezoneW";
       h = "0.044 * safezoneH";
+      colorBackground[]={0,0,0,0.5};
     };
     class armatak_gui_module_zeus_core_dialog_address_port_edit: RscEdit {
       idc = 14001;
@@ -64,6 +66,7 @@ class armatak_zeus_core_module_dialog {
       y = "0.522 * safezoneH + safezoneY";
       w = "0.20625 * safezoneW";
       h = "0.044 * safezoneH";
+      colorBackground[]={0,0,0,0.5};
     };
     class armatak_gui_module_zeus_core_dialog_address_text: RscText {
       idc = 1000;
@@ -97,6 +100,74 @@ class armatak_zeus_core_module_dialog {
       x = "0.5 * safezoneW + safezoneX";
       y = "0.577 * safezoneH + safezoneY";
       w = "0.0464063 * safezoneW";
+      h = "0.055 * safezoneH";
+    };
+  };
+};
+
+class armatak_zeus_custom_marker_dialog {
+  idd = 990991;
+  movingEnable = 0;
+
+  class Controls {
+    class RscFrame_1800: RscFrame
+    {
+      idc = 1800;
+      x = "0.37625 * safezoneW + safezoneX";
+      y = "0.357 * safezoneH + safezoneY";
+      w = "0.237187 * safezoneW";
+      h = "0.275 * safezoneH";
+    };
+    class RscEdit_1400: RscEdit
+    {
+      idc = 1400;
+      x = "0.386562 * safezoneW + safezoneX";
+      y = "0.423 * safezoneH + safezoneY";
+      w = "0.216563 * safezoneW";
+      h = "0.044 * safezoneH";
+    };
+    class RscText_1000: RscText
+    {
+      idc = 1000;
+      text = "Entity Callsign";
+      x = "0.386562 * safezoneW + safezoneX";
+      y = "0.379 * safezoneH + safezoneY";
+      w = "0.216563 * safezoneW";
+      h = "0.044 * safezoneH";
+    };
+    class RscText_1001: RscText
+    {
+      idc = 1001;
+      text = "Entity Type (only for vehicles)";
+      x = "0.386562 * safezoneW + safezoneX";
+      y = "0.467 * safezoneH + safezoneY";
+      w = "0.216563 * safezoneW";
+      h = "0.044 * safezoneH";
+    };
+    class RscEdit_1401: RscEdit
+    {
+      idc = 1401;
+      x = "0.386562 * safezoneW + safezoneX";
+      y = "0.511 * safezoneH + safezoneY";
+      w = "0.216563 * safezoneW";
+      h = "0.044 * safezoneH";
+    };
+    class RscButton_1600: RscButton
+    {
+      idc = 1600;
+      text = "Cancel";
+      x = "0.551562 * safezoneW + safezoneX";
+      y = "0.566 * safezoneH + safezoneY";
+      w = "0.0515625 * safezoneW";
+      h = "0.055 * safezoneH";
+    };
+    class RscButton_1601: RscButton
+    {
+      idc = 1601;
+      text = "OK";
+      x = "0.489687 * safezoneW + safezoneX";
+      y = "0.566 * safezoneH + safezoneY";
+      w = "0.0515625 * safezoneW";
       h = "0.055 * safezoneH";
     };
   };
