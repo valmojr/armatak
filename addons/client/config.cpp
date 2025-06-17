d9da877da6efa43de12ef7f8a2434c26a58908b3
Valmo Trindade <val_mo_jr@hotmail.com>
2025-06-17 03:43:07 -0300

    added ace self action to create the eud connection dialog

--- addons/client/config.cpp
+++ addons/client/config.cpp
@@ -16,4 +16,20 @@ class CfgPatches {
   };
 };
 
+class CfgVehicles {
+    class Man;
+    class CAManBase: Man {
+        class ACE_SelfActions {
+            class danceParty {
+                displayName = "Connect to EUD";
+                condition = "!(player getVariable ['armatak_client_eudConnected', false])";
+                exceptions[] = {};
+                statement = "createDialog 'armatak_udp_socket_start_dialog'";
+                icon = "";
+            };
+        };
+    };
+};
+
 #include "CfgEventHandlers.hpp"
+#include "dialog.hpp"
