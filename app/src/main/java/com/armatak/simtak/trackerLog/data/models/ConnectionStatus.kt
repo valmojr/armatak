30be94358147e6fc38371486c13f5d64bf804d12
Valmo Trindade <val_mo_jr@hotmail.com>
2025-06-17 02:12:28 -0300

    FINALLY removed the SIMTAK application for my project, it was a good start, but it's time to move on and i got it native on ATAK, thanks @Andonyth for the help

--- app/src/main/java/com/armatak/simtak/trackerLog/data/models/ConnectionStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.armatak.simtak.trackerLog.data.models
-
-sealed interface ConnectionStatus {
-    data object Connected : ConnectionStatus
-    data object Disconnected : ConnectionStatus
-    data object OnReconnect : ConnectionStatus
-    data object Awaiting : ConnectionStatus
-    data object InitialValue : ConnectionStatus
-}
