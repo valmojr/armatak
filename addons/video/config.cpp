15129bb3442248be8462f4a537ee8da7600b9ac7
Valmo Trindade <val_mo_jr@hotmail.com>
2025-06-23 02:25:36 -0300

    removed useless mod metadata from addons/video config

--- addons/video/config.cpp
+++ addons/video/config.cpp
@@ -19,24 +19,5 @@ class CfgPatches {
   };
 };
 
-class CfgMods {
-    class PREFIX {
-      name = "Arma Team Awareness Kit";
-      author = PROJECT_AUTHOR;
-      logo = "logo_ca.paa";
-      logoOver = "logo_hover_ca.paa";
-      tooltip	= "ARMATAK";
-      tooltipOwned = "ARMATAK";
-      picture = "picture.paa";
-      actionName = "GitHub";
-      action = "https://github.com/valmojr/armatak";
-      overview = "ARMATAK Addons allows Arma 3 sessions to be parsed to TAK Clients";
-      hideName = 0;
-      hidePicture	= 0;
-      dlcColor[] = { 0.23, 0.39, 0.30, 1 };
-      logoSmall	= "logo_small_ca.paa";
-    };
-};
-
 #include "CfgEventHandlers.hpp"
 #include "CfgVehicles.hpp"
