a18343b81de89a0e68db2c158131de943bfd7015
Valmo <valmojltrindade@gmail.com>
2026-03-24 14:03:28 -0300

    Commented video module

--- addons/video/config.cpp
+++ addons/video/config.cpp
@@ -4,7 +4,7 @@ class CfgPatches {
   class ADDON {
     name = COMPONENT_NAME;
     units[] = {
-      QGVAR(videoModule)
+      //QGVAR(videoModule)
     };
     weapons[] = {};
     requiredAddons[] = {
